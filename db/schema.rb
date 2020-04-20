baae0f774e80c284d9de3cde44b70bcb1025290d
Jonathan Bowman <jonathanrbowman@me.com>
2020-04-19 21:10:07 -0400

    Do a better job at adding the migration for services

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_005944) do
     t.boolean "is_offering", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_services_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
